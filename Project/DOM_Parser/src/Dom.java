abe35f17ffc1fed51a2b530df63320dcc1dd1480
Akroma188 <denis.dr4@gmail.com>
2018-04-30 02:00:57 +0100

    Updated parser (to be continued)

--- Project/DOM_Parser/src/Dom.java
+++ Project/DOM_Parser/src/Dom.java
@@ -1,8 +1,31 @@
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class Dom {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("data1.xml");
+			NodeList taglist = doc.getElementsByTagName(arg0)
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
