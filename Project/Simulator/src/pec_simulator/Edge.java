b17b3edd1625f0f287259b4c6e026d350e12ca5a
jmcoftrindade <38080771+jmcoftrindade@users.noreply.github.com>
2018-05-03 18:07:52 +0100

    Add files via upload
    Grid feita (?)

---
+++ Project/Simulator/src/pec_simulator/Edge.java
@@ -0,0 +1,25 @@
+package simulator;
+
+public class Edge {
+    private Point adjacent;
+    private int cost = -1;
+
+    // constructor
+    Edge (Point adjacent, int cost) {
+        this.adjacent = adjacent;
+        this.cost = cost;
+    }
+
+    // getters
+    public Point getAdjacent() {
+        return adjacent;
+    }
+    public int getCost() {
+        return cost;
+    }
+
+    // setter
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
