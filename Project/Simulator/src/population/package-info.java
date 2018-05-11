ecef6f862a08e0d3f70478e0a419391592171bb5
tiagomiotto <tiago.miotto@hotmail.com>
2018-05-11 13:03:26 +0100

    Missing grid, igrid, and parser, grid-package javadoc

---
+++ Project/Simulator/src/population/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Population package
+ * A package containing the necessary classes and methods to store and
+ * implement the evolution of individuals throughout a stochastic simulation
+ * as well as a Comparator implementations to sort any kind of List from the
+ * JDK library.
+ *
+ * @author Tiago Miotto
+ * @version 1.0
+ */
+
+package population;
