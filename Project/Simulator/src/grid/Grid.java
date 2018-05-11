455656f4386a4af6ef96ffca58f1cd2d119f8e48
jmcoftrindade <38080771+jmcoftrindade@users.noreply.github.com>
2018-05-11 17:17:35 +0100

    Javadoc ready for Grid package

--- Project/Simulator/src/grid/Grid.java
+++ Project/Simulator/src/grid/Grid.java
@@ -2,35 +2,56 @@ package grid;
 
 import java.util.List;
 
+/**
+ * Used to map the points, with obstacles, tuples, initial and final points
+ *
+ * @author Group 18
+ */
+
 public class Grid {
+    /**
+     * cols stores the specified number of columns
+     * rows stores the specified number of rows
+     */
     private int cols;
     private int rows;
+    /**
+     * gridMatrix  is (rows x cols) array that representes the grid as a matrix
+     */
     private Point[][] gridMatrix;
+    /**
+     * initCoord stores the initial point for individuals for a specified map
+     * finCoord stores the final point for individuals for a specified map
+     */
     private Coordinates initCoord, finCoord;
-    /*
-    private Tuple[] tuples;
-    private Point[] obstacles;
-    */
 
-    // constructor
-    public Grid(int cols, int rows/*Tuple[] costZones, Obstacle[] obstList*/) {
+    /**
+     * Constructor for class Grid
+     * @param cols
+     *              specified number of columns
+     * @param rows
+     *              specified number of rows
+     */
+    public Grid(int cols, int rows) {
         this.cols = cols;
         this.rows = rows;
-        /*
-        this.tuples = costZones;
-        this.obstacles = obstList;
-        */
         initCoord = new Coordinates(0, 0);
         finCoord = new Coordinates(0, 0);
+
+         // initializes the gridMatrix
         this.gridMatrix = new Point[cols][rows];
         for (int i = 0; i < cols; i++)
             for (int j = 0; j < rows; j++)
                 gridMatrix[i][j] = null;
     }
 
+    /**
+     * Used to update the gridMatrix after parsing the file
+     */
     public void updateGrid() {
         this.gridMatrix = new Point[cols][rows];
     }
+
     public Coordinates getInitCoord() {
         return initCoord;
     }
@@ -71,7 +92,11 @@ public class Grid {
     }
 
 
-    // toString description
+    /**
+     * Used to obtain a textual description of the Grid
+     * @return
+     *              string with said description
+     */
     @Override
     public String toString() {
         String outString = "Grid: ";
@@ -83,19 +108,19 @@ public class Grid {
                 outString += "\n(" + (x + 1) + "," + (y + 1) + ")" + "obstáculo:" + (aPoint.obsFlag()) + "\n";
 
                 if (!(aPoint.obsFlag())) {
-                    // norte
+                    // above
                     if (y < rows - 1 && aPoint.adjFlag(gridMatrix[x][y + 1])) {
                         outString += "Norte: " + aPoint.getAdjCost(gridMatrix[x][y + 1]) + "\n";
                     }
-                    // sul
+                    // below
                     if (y > 0 && aPoint.adjFlag(gridMatrix[x][y - 1])) {
                         outString += "Sul: " + aPoint.getAdjCost(gridMatrix[x][y - 1]) + "\n";
                     }
-                    // este
+                    // right
                     if (x < cols - 1 && aPoint.adjFlag(gridMatrix[x + 1][y])) {
                         outString += "Este: " + aPoint.getAdjCost(gridMatrix[x + 1][y]) + "\n";
                     }
-                    // oeste
+                    // left
                     if (x > 0 && aPoint.adjFlag(gridMatrix[x - 1][y])) {
                         outString += "Oeste: " + aPoint.getAdjCost(gridMatrix[x - 1][y]) + "\n";
                     }
@@ -105,41 +130,51 @@ public class Grid {
         return outString;
     }
 
-
-    // getter de um Point da gridMatrix correspondente às Coordinates dadas
+    /**
+     * Gets a Point from gridMatrix corresponding to the specified Coordinates
+     * @param aCoord
+     *              specified Coordinates
+     * @return
+     *              corresponding gridMatrix Point
+     */
     public Point getPoint(Coordinates aCoord) {
-        return gridMatrix[aCoord.getX()-1][aCoord.getY()-1];
-    }
-
-    public Point getSpecPoint(Coordinates aCoord) {
         return gridMatrix[aCoord.getX()][aCoord.getY()];
     }
 
-    // adiciona um Point à Grid
+
+    /**
+     * Adds a specified Point to the gridMatrix
+     * @param aPoint
+     *              specified Point
+     */
     public void addPoint(Point aPoint) {
         gridMatrix[aPoint.getX()][aPoint.getY()] = aPoint;
     }
 
     // adiciona os adjacentes com custo 1 aos pontos da Grid
+
+    /**
+     * Fills the gridMatrix with the adjacent Points, with a default cost of 1
+     */
     public void fillGridAdj() {
-        // percorre a gridMatrix
+        // cycles through the gridMatrix
         for (int x = 0; x < cols; x++) {
             for (int y = 0; y < rows; y++) {
-                // se o ponto não for obstáculo
+                // if the Point isn't an obstacle
                 if (!(gridMatrix[x][y].obsFlag())) {
-                    // norte
+                    // above the Point
                     if (y < rows - 1 && !(gridMatrix[x][y + 1].obsFlag())) {
                         gridMatrix[x][y].addAdj(gridMatrix[x][y + 1], 1);
                     }
-                    // sul
+                    // below the Point
                     if (y > 0 && !(gridMatrix[x][y - 1].obsFlag())) {
                         gridMatrix[x][y].addAdj(gridMatrix[x][y - 1], 1);
                     }
-                    // este
+                    // right of the Point
                     if (x < cols - 1 && !(gridMatrix[x + 1][y].obsFlag())) {
                         gridMatrix[x][y].addAdj(gridMatrix[x + 1][y], 1);
                     }
-                    // oeste
+                    // left of the Point
                     if (x > 0 && !(gridMatrix[x - 1][y].obsFlag())) {
                         gridMatrix[x][y].addAdj(gridMatrix[x - 1][y], 1);
                     }
@@ -148,24 +183,31 @@ public class Grid {
         }
     }
 
-    // adiciona as zonas de custo Tuples à Grid
+    /**
+     * Fills the grid with the special cost zones (Tuple)
+     * @param tuples
+     *              array of the specified special cost zones
+     */
     public void fillGridTuples(Tuple[] tuples) {
         int xi, yi, xf, yf;
         int cost;
         Point pointA, pointB;
 
+        // cycles throught the Array of Tuples
         for (Tuple tuple : tuples) {
+            // applies the variable change
             xi = tuple.getiCoord().getX() - 1;
             yi = tuple.getiCoord().getY() - 1;
             xf = tuple.getfCoord().getX() - 1;
             yf = tuple.getfCoord().getY() - 1;
             cost = tuple.getCost();
 
-            // direcção horizontal
+            // cycles through the horizontal direction
             for (int x = xi; x <= xf; x++) {
+                // defines Points on the same row
                 pointA = gridMatrix[x][yi];
                 pointB = gridMatrix[x][yf];
-                // este
+                // right
                 if (x < xf) {
                     if (pointA.getAdjCost(gridMatrix[x + 1][yi]) < cost && !(pointA.obsFlag())) {
                         pointA.setAdjCost(gridMatrix[x + 1][yi], cost);
@@ -174,7 +216,7 @@ public class Grid {
                         pointB.setAdjCost(gridMatrix[x + 1][yf], cost);
                     }
                 }
-                // oeste
+                // left
                 if (x > xi) {
                     if (pointA.getAdjCost(gridMatrix[x - 1][yi]) < cost && !(pointA.obsFlag())) {
                         pointA.setAdjCost(gridMatrix[x - 1][yi], cost);
@@ -184,11 +226,12 @@ public class Grid {
                     }
                 }
             }
-            // direcção vertical
+            // cycles through the vertical direction
             for (int y = yi; y <= yf; y++) {
+                // defines Points on the same column
                 pointA = gridMatrix[xi][y];
                 pointB = gridMatrix[xf][y];
-                // norte
+                // above
                 if (y < yf) {
                     if (pointA.getAdjCost(gridMatrix[xi][y + 1]) < cost && !(pointA.obsFlag())) {
                         pointA.setAdjCost(gridMatrix[xi][y + 1], cost);
@@ -197,7 +240,7 @@ public class Grid {
                         pointB.setAdjCost(gridMatrix[xf][y + 1], cost);
                     }
                 }
-                // sul
+                // below
                 if (y > yi) {
                     if (pointA.getAdjCost(gridMatrix[xi][y - 1]) < cost && !(pointA.obsFlag())) {
                         pointA.setAdjCost(gridMatrix[xi][y - 1], cost);
@@ -212,46 +255,64 @@ public class Grid {
 
 
     // matrix builder
+
+    /**
+     * Builds a bare-bones gridMatrix version of map
+     * @param obstacles
+     *              array of specified Obstacles
+     */
     public void buildGrid(Coordinates[] obstacles) {
         Point aPoint;
 
-        // posiciona os obstáculos
+        // places the obstacles, applying the variable change
         for (Coordinates obstacle : obstacles) {
             aPoint = new Obstacle(obstacle.getX() - 1, obstacle.getY() - 1);
             addPoint(aPoint);
         }
 
-        // preenche o resto dos pontos livres
+        // fills the rest of the gridMatrix with "free" (non-obstacle) Points
         for (int x = 0; x < cols; x++) {
             for (int y = 0; y < rows; y++) {
-
+                // checks if the Point is "free" (non-obstacle)
                 if (gridMatrix[x][y] == null) {
-
                     aPoint = new Point(x, y);
                     addPoint(aPoint);
                 }
             }
         }
 
-        // preenche os pontos adjacentes de cada ponto da gridMatrix
+        // fills the gridMatrix with the adjacent Points of the "free" Points
         this.fillGridAdj();
     }
 
-    // calcula a distância até ao ponto final
+    /**
+     * Calculates the distance from specified Coordinates to the final Coordinates
+     * @param coord
+     *              specified Coordinates
+     * @return
+     *              distance to final Coordinates
+     */
     public double dist(Coordinates coord) {
         return Math.sqrt(Math.pow(this.finCoord.getX() - coord.getX(), 2) + Math.pow(this.finCoord.getY() - coord.getY(), 2));
     }
 
+    /**
+     * Calculates the total cost of an specified Individual's path
+     * @param path
+     *              array of Coordinates corresponding to the specified path
+     * @param length
+     *              integer corresponding to the length of the specified path
+     * @return
+     *              integer cost of the specified path
+     */
     public int cost(List<Coordinates> path, int length) {
-        //TODO cost calculator
         int i, cost = 0;
 
+        // cycles through the path, updating the value of the path cost
         for (i = 0; i < path.size() - 1; i++) {
             cost = cost + gridMatrix[path.get(i).getX()][path.get(i).getY()].getAdjCost(gridMatrix[path.get(i + 1).getX()][path.get(i + 1).getY()]);
         }
 
-
         return cost;
     }
-
 }
--- Project/Simulator/src/grid/Point.java
+++ Project/Simulator/src/grid/Point.java
@@ -2,27 +2,59 @@ package grid;
 
 import java.util.LinkedList;
 
+/**
+ * Used to represent a "free" point of the specified map
+ *
+ * @author Group 18
+ */
 public class Point extends Coordinates {
 
+    // listAdj array that stores the adjacent Points for a specified Point
     private LinkedList<Edge> listAdj = new LinkedList<Edge>();
 
     // constructor
+
+    /**
+     * Constructor for class Point
+     * @param x
+     *              integer corresponding to the x coordinate
+     * @param y
+     *              integer corresponding to the y coordinate
+     */
     public Point(int x, int y) {
         super(x, y);
     }
 
-    // por definição não é obstáculo
+    /**
+     * Indicates that a specified Point is "free" (non-obstacle)
+     * @return
+     *              boolean indicating that it is "free"
+     */
     public boolean obsFlag() {
         return false;
     }
 
-    // adiciona um ponto à sua lista de adjacentes
+    /**
+     * Adds a Point to the specified Point's adjacent list
+     * @param adjacent
+     *              Point to be added to the specified Point's list
+     * @param costAdj
+     *              integer corresponding to the cost of the connection between Point to be added and specified Point
+     */
     public void addAdj(Point adjacent, int costAdj) {
         Edge adjPoint = new Edge(adjacent, costAdj);
         listAdj.addFirst(adjPoint);
     }
 
     // verifica se um ponto é adjacente a outro
+
+    /**
+     * Indicates if a Point is adjacent to a specified Point
+     * @param adjacent
+     *              Point to be checked if it's adjacent to the specified Point
+     * @return
+     *              boolean that indicates if they are adjacent (true)
+     */
     public boolean adjFlag(Point adjacent) {
         for (Edge edge : listAdj) {
             if (edge.getAdjacent() == adjacent) {
@@ -33,6 +65,14 @@ public class Point extends Coordinates {
     }
 
     // getter do custo do adjacente
+
+    /**
+     * Gets the cost of the connection between the adjacent Point and the specified Point
+     * @param adjacent
+     *              Point adjacent to the specified Point
+     * @return
+     *              Cost of their connection
+     */
     public int getAdjCost(Point adjacent) {
         for (Edge edge : listAdj) {
             if (edge.getAdjacent() == adjacent) {
@@ -42,7 +82,13 @@ public class Point extends Coordinates {
         return 0;
     }
 
-    // setter do custo do adjacente
+    /**
+     * Sets the cost of the connection between a specified Point and its adjacent Point
+     * @param adjacent
+     *              Point adjacent to the specified Point
+     * @param costAdj
+     *              Cost of their connection
+     */
     public void setAdjCost(Point adjacent, int costAdj) {
         for (Edge edge : listAdj) {
             if (edge.getAdjacent() == adjacent) {
@@ -51,10 +97,20 @@ public class Point extends Coordinates {
         }
     }
 
+    /**
+     * Gets the total number of adjacent Points to a specified Point
+     * @return
+     *              integer number of adjacent Points
+     */
     public int getAdjNum() {
         return listAdj.size();
     }
 
+    /**
+     * Gets the list of adjacent Points to a specified Point
+     * @return
+     *              LinkedList of adjacent Points
+     */
     public LinkedList<Edge> getListAdj() {
         return listAdj;
     }
