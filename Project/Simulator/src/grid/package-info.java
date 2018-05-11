a9fcc6c5abbc07491687568dd1f7db8ad23dce8b
tiagomiotto <tiago.miotto@hotmail.com>
2018-05-11 13:06:01 +0100

    Missing grid, igrid, and parser javadoc

--- Project/Simulator/src/grid/package-info.java
+++ Project/Simulator/src/grid/package-info.java
@@ -1,1 +1,13 @@
+/**
+ * Grid package
+ * A package containing the necessary classes and methods to build a grid with
+ * obstacles and tuples with special costs, as well as providing classes to easily
+ * identify the position of an individual in the Grid, and calculating the cost
+ * of the path it has taken throughout his life
+ *
+ * @author Tiago Miotto
+ * @version 1.0
+ * @see simulator.Simulator
+ * @see population.Individual
+ */
 package grid;
